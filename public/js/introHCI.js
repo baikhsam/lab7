bee19b7873d89dfc6dd6ce70dae9704eaac7b6e9
Sam Baik <baikhsam@gmail.com>
2020-02-20 14:17:30 -0800

    added viewalt

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -10,4 +10,11 @@ $(document).ready(function() {
  */
 function initializePage() {
 	// your code here
+	$('.likeBtn').click(clickLikeBtn);
+}
+
+function clickLikeBtn(e) {
+	e.preventDefault();
+	ga('create', 'UA-158763354-1', 'auto');
+	ga('send', 'event', 'like', 'action');
 }
